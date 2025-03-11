9e097201111d54b71e5529c004fdcaede1ea8b9f
gulerhasan <hasanguler222@gmail.com>
2025-03-11 22:54:40 +0300

    initial commit

---
+++ sinavrotasi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sinavrotasi
+//
+//  Created by Hasan Güler on 11.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sinavrotasi/sinavrotasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sinavrotasiApp.swift
+//  sinavrotasi
+//
+//  Created by Hasan Güler on 11.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct sinavrotasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
